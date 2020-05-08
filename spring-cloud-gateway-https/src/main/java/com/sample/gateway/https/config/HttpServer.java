3ff13198392044e561b726c28fd0bdc297431386
kangxinli <kangxinli0201@163.com>
2020-05-08 10:15:20 +0800

    fix: geteway

---
+++ spring-cloud-gateway-https/src/main/java/com/sample/gateway/https/config/HttpServer.java
@@ -0,0 +1,39 @@
+package com.sample.gateway.https.config;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.HttpHandler;
+
+/**
+ * 
+ * @function   http访问配置类
+ *
+ */
+@Configuration
+public class HttpServer {
+	@Autowired
+	private HttpHandler httpHandler;
+	
+	private WebServer webServer;
+	
+	@Value("${http.port}")
+	private int httpPort;
+	
+	@PostConstruct
+	public void start() {
+		NettyReactiveWebServerFactory factory = new NettyReactiveWebServerFactory(httpPort);
+		WebServer webServer = factory.getWebServer(httpHandler);
+		webServer.start();
+	}
+	
+	@PreDestroy
+	public void stop() {
+		webServer.stop();
+	}
+}
---
+++ spring-cloud-gateway-ribbon/provider3/src/main/java/com/sample/app/Provider3Application.java
@@ -0,0 +1,13 @@
+package com.sample.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Provider3Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Provider3Application.class, args);
+	}
+
+}
